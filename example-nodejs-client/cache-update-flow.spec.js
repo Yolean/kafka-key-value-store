fa7112aae663277afc761fbf3544b414dccbf3c5
Staffan Olsson <staffan@repos.se>
2019-02-11 22:09:20 +0100

    Draft test setup

--- example-nodejs-client/cache-update-flow.spec.js
+++ example-nodejs-client/cache-update-flow.spec.js
@@ -25,6 +25,17 @@ describe("A complete cache update flow", () => {
 
   });
 
+  test("Check that pixy is online", done => {
+
+    request('http://pixy', function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      done();
+    });
+
+  });
+
   it("Starts with a produce to Pixy", async () => {
 
   });
