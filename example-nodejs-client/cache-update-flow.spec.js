3c97ae2d65d660cbb809fd2966ffce468a7f3729
Staffan Olsson <staffan@repos.se>
2019-02-11 16:40:27 +0100

    Adds a draft acceptance test spec,
    for the alternative github.com/Yolean/kafka-cache implementation.

---
+++ example-nodejs-client/cache-update-flow.spec.js
@@ -0,0 +1,42 @@
+
+
+
+// First set up a mock web server that matches the 
+
+// Produce message to pixy
+
+describe("A complete cache update flow", () => {
+
+  it("Starts with a produce to Pixy", async () => {
+
+  });
+
+  it("Waits for ack from Pixy", async () => {
+
+  });
+
+  it("Gets the produced offset from Pixy's response", () => {
+
+  });
+
+  it("Waits for the cache to notify onUpdate", async () => {
+
+  });
+
+  it("When the notify handler returns non-200 gets another notify", async () => {
+
+  });
+
+  it("Includes the updated key and the offset at which the update happened", () => {
+
+  });
+
+  it("The message is now retrievable from KV using the key", async () => {
+
+  });
+
+  it("The current cache offset (global, not the key's latest) is avialable from KV", async () => {
+
+  });
+
+});
