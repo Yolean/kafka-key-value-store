4bb85554eeaf7a48dd0445f9f48060d2338a49b9
Staffan Olsson <staffan@repos.se>
2019-02-11 23:32:02 +0100

    Adds the first produce-consume test

--- example-nodejs-client/cache-update-flow.spec.js
+++ example-nodejs-client/cache-update-flow.spec.js
@@ -1,7 +1,9 @@
 
 const {
-  PIXY_HOST = 'localhost:18090',
-  CACHE1_HOST = 'localhost:18081'
+  PIXY_HOST = 'http://localhost:18090',
+  CACHE1_HOST = 'http://localhost:18081',
+  TOPIC1_NAME = 'topic1',
+  TEST_ID = '' + new Date().toISOString()
 } = process.env;
 
 const fetch = require('node-fetch');
@@ -24,20 +26,51 @@ describe("A complete cache update flow", () => {
   });
 
   test("Check that pixy is online", async () => {
-    const response = await fetch(`http://${PIXY_HOST}`);
+    const response = await fetch(PIXY_HOST);
     expect(response.status).toEqual(404);
   });
 
   it("Starts with a produce to Pixy", async () => {
-
+    const response = await fetch(`${PIXY_HOST}/topics/${TOPIC1_NAME}/messages`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({test: TEST_ID, n: 1})
+    });
+    expect(response.ok).toBeTruthy();
+    expect(await response.json()).toEqual({});
   });
+
+  let syncResponse = null;
 
   it("Waits for ack from Pixy", async () => {
-
+    syncResponse = await fetch(`${PIXY_HOST}/topics/${TOPIC1_NAME}/messages?key=test1&sync`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({test: TEST_ID, n: 2})
+    });
+    expect(syncResponse.ok).toBeTruthy();
   });
 
-  it("Gets the produced offset from Pixy's response", () => {
+  it("Gets the produced offset from Pixy's response", async () => {
+    expect(syncResponse).toBeTruthy();
+    const result = await syncResponse.json();
+    expect(result).toBeTruthy();
+    expect(result.partition).toEqual(0);
+    expect(result.offset).toBeGreaterThan(1);
+  });
 
+  it("Until onUpdate is implemented we just have to wait here", done => {
+    setTimeout(done, 500);
+  });
+
+  it("Finds the value in the cache", async () => {
+    const response = await fetch(`${CACHE1_HOST}/messages/test1`);
+    expect(await response.json()).toEqual({"key":"test1","value":`{\"test\":\"${TEST_ID}\",\"n\":2}`});
+    expect(response.ok).toBeTruthy();
   });
 
   it("Waits for the cache to notify onUpdate", async () => {
